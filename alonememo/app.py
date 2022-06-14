de49ecd60c354292937e298ee15159a6753da4d8
snailweeds <hyunji202@naver.com>
2022-06-14 22:12:14 +0900

    alonememogetpostpractice

--- alonememo/app.py
+++ alonememo/app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
-
 import requests
 from bs4 import BeautifulSoup
-
 from pymongo import MongoClient
+
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
